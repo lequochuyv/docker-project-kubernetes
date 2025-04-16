23349b2032af6520c59fe2b3b1c9f6e05848120a
lequochuyv <lequochuyv@gmail.com>
2025-04-16 22:37:32 +0700

    front-end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -10,14 +10,27 @@ app.get('/', (req, res) => {
   res.send('Hello World10!')
 })
 
-app.get('/users/list', (req, res) => {
-  console.log('Fetching users from the database');
-  var sql = 'SELECT * FROM users';
-  db.query(sql, (err, result) => {
-    if (err) throw err;
+app.get('/users/list', async (req, res) => {
+  try {
+    console.log('Fetching users from the database');
+    var sql = 'SELECT * FROM users';
+    const [result] = await db.query(sql)
     res.send(result);
-  });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Error fetching users');
+  }
 })
+
+app.get('/health-check', async (req, res) => {
+  try {
+    await db.query('SELECT 1');
+    res.status(200).send('OK');
+  } catch (error) {
+    console.error('Health check failed:', error);
+    res.status(500).send('DB Connection Failed');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
